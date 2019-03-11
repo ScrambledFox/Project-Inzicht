b4b8a675fa66f90e944502ce5eeab000d4c2304b
Joris Lodewijks <info@jorislodewijks.nl>
2019-03-11 23:32:11 +0100

    EOD 1
    Nothing major.

---
+++ Project Inzicht/Assets/Scripts/CameraOnTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOnTarget : MonoBehaviour {
+
+    public Transform target;
+
+    private void Update () {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+
+}
